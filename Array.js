fc1c1b11e95544d71e9981e9eb6d2d6f4f13a7d9
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-13 11:11:43 -0600

    Update and rename Array to Array.js

---
+++ Array.js
@@ -0,0 +1,9 @@
+// Daniel Davis, PracticeArray, make an array and populate it with values 1-10 and print them out//
+
+var array = [0,1,2,3,4,5,6,7,8,9]; // constructs an array
+
+for(var i=0, i<=array.length, i++) { // for loop that goes through the length of the array
+  if ([i] %2) { // if the element in the array at said location is mod 2
+    console.log([i]); // print it out on the screen
+  }// end of if statement
+}// end of for loop
