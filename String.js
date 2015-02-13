1665fddef5a46e69fa1d79de0fd746d0a404ade5
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-13 11:52:21 -0600

    Create String.js

---
+++ String.js
@@ -0,0 +1,8 @@
+// Daniel Davis, String, prints out the 4th, 7th, 10th characters in that string//
+
+var string = 'Your face is a zombie, yo'
+
+
+if (string.charat(4) || string.charat(7) || string.charat(10)) {
+
+}// end of if
